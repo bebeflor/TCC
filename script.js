fde64dd3dc2b79a5bf1126f4b1eae68040ef1058
EA - Ana Beatriz Alves De Siqueira <siqueira_beatriz@educadventista.org>
2025-05-22 08:45:10 -0300

    atualização fabio

---
+++ script.js
@@ -0,0 +1,12 @@
+// script.js
+
+function abrirMenu() {
+    document.getElementById("menu-lateral").style.left = "0";
+  }
+  
+  function fecharMenu() {
+    document.getElementById("menu-lateral").style.left = "-250px";
+  }
+  
+  console.log("Site Vitaliz carregado com sucesso!");
+  
